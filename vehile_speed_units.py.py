4dbb60750eb1766c946971c4eef4d119f5ced4c8
Parchuri mahesh babu <136172133+parchurimaheshbabu@users.noreply.github.com>
2023-12-10 01:06:18 +0530

    Update vehile_speed_units.py.py

--- vehile_speed_units.py.py
+++ vehile_speed_units.py.py
@@ -1,27 +1,27 @@
-# class car:
-#     def __init__(self,sp,unit):
-#         self.speed=sp
-#         self.unit=unit
-#         print("Car with maximium speed of",sp,unit)
-# class boat:
-#     def __init__(self,sp):
-#         self.speed=sp
-#         print("Boat with maximium speed of",sp,"knots") 
+ class car:
+     def __init__(self,sp,unit):
+         self.speed=sp
+         self.unit=unit
+         print("Car with maximium speed of",sp,unit)
+ class boat:
+     def __init__(self,sp):
+         self.speed=sp
+         print("Boat with maximium speed of",sp,"knots") 
 
-# l=[]
-# k=int(input("enter quwry"))
-# for i in range(k):
-#     k=input("")
-#     l.append(list(k.split()))
-# for i in l:
+ l=[]
+ k=int(input("enter quwry"))
+ for i in range(k):
+     k=input("")
+     l.append(list(k.split()))
+ for i in l:
 
-#     for j in i:
-#         if j=='car':
-#             veh,sp,unit=i
-#             obj=car(sp ,unit)
-#         elif j=='boat':
-#             veh,sp=i
-#             obj=boat(sp)
+     for j in i:
+         if j=='car':
+             veh,sp,unit=i
+             obj=car(sp ,unit)
+         elif j=='boat':
+             veh,sp=i
+             obj=boat(sp)
       
 ma=[['add', 'a'], ['add', 'b'], ['total']]
 for i in ma:
